d4536fb593e31a43b099ee3ae791bc103c6ffc9d
tquangdo <trangia61@gmail.com>
2020-09-24 01:18:58 +0900

    Updates

---
+++ precache-manifest.f96a52232ba0e788db0102f234b5f9f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c83e3fdfbe3cd78860851448aa2bad",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "821d0c35193a4357bc16",
+    "url": "/airbnb-clone/static/css/2.a3735241.chunk.css"
+  },
+  {
+    "revision": "85d7004e4f04eb85143a",
+    "url": "/airbnb-clone/static/css/main.417e400b.chunk.css"
+  },
+  {
+    "revision": "821d0c35193a4357bc16",
+    "url": "/airbnb-clone/static/js/2.2b695b54.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/airbnb-clone/static/js/2.2b695b54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d7004e4f04eb85143a",
+    "url": "/airbnb-clone/static/js/main.fb1fa310.chunk.js"
+  },
+  {
+    "revision": "f358a9c4d5787a3a04f2",
+    "url": "/airbnb-clone/static/js/runtime-main.fc586be8.js"
+  }
+]);
